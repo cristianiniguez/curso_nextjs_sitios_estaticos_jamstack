8e650d6df7a265753a6b49ee122731efaf5fd65e
cristianiniguez <felizpc1@gmail.com>
2021-09-25 20:11:10 -0400

    arquitectura del proyecto: graphql api y autogeneracion de codigo

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,19 @@
+import { useEffect } from 'react'
 import { Grid } from '@ui/Grid'
 import { Button } from '@ui/Button'
 import { Typography } from '@ui/Typography'
 import { Layout } from '@components/Layout'
+import { getPlantList } from '@api'
 
 export default function Home() {
+  useEffect(() => {
+    getPlantList({ limit: 10 }).then((data) => {
+      data.forEach((item) => {
+        console.log(item.author.fullName)
+      })
+    })
+  }, [])
+
   return (
     <Layout>
       <Typography variant="h2" className="text-center">

841068904c86c8a88d05160c799aa8869a5ed261
cristianiniguez <felizpc1@gmail.com>
2021-09-27 19:45:04 -0400

    static generation: moviendo al servidor - getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,38 @@
-import { useState, useEffect } from 'react'
+import { GetStaticProps, InferGetServerSidePropsType } from 'next'
 import { Layout } from '@components/Layout'
+import { Hero } from '@components/Hero'
+import { Authors } from '@components/Authors'
 import { PlantCollection } from '@components/PlantCollection'
 import { getPlantList } from '@api'
 
-export default function Home() {
-  const [data, setData] = useState<Plant[]>([])
+type HomeProps = { plants: Plant[] }
 
-  useEffect(() => {
-    getPlantList({ limit: 10 }).then((receivedData) => setData(receivedData))
-  }, [])
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+  const plants = await getPlantList({ limit: 10 })
 
+  return {
+    props: {
+      plants,
+    },
+  }
+}
+
+export default function Home({
+  plants,
+}: InferGetServerSidePropsType<typeof getStaticProps>) {
   return (
     <Layout>
-      <PlantCollection plants={data} variant="square" />
+      <Hero {...plants[0]} className="mb-20" />
+      <Authors className="mb-10" />
+      <PlantCollection
+        plants={plants.slice(1, 3)}
+        variant="vertical"
+        className="mb-24"
+      />
+      <PlantCollection
+        plants={plants.length > 8 ? plants.slice(3, 9) : plants.slice(3)}
+        variant="square"
+      />
     </Layout>
   )
 }
